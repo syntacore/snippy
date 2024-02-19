d394a56b38f6529ef31e90c962cd42e7ad8e3335
Ivan Shumakov <ivan.shumakov@syntacore.com>
2024-02-19 13:24:12 +0300

    [snippy] Add selfcheck ref-values storage option

---
+++ llvm/tools/llvm-snippy/include/snippy/Generator/SelfcheckMode.h
@@ -0,0 +1,17 @@
+//===-- SelfcheckMode.h -----------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+namespace llvm::snippy {
+
+enum class SelfcheckRefValueStorageType {
+  Code,
+};
+
+} // namespace llvm::snippy
