ba208c5dd90541c9a1165364fb3db7ab4a284e20
Yuly Tarasov <yuly.tarasov@syntacore.com>
2024-02-21 19:56:34 +0300

    [snippy] Add possibility to generate only consecutive loops

--- llvm/tools/llvm-snippy/lib/ConsecutiveLoopsVerifierPass.cpp
+++ llvm/tools/llvm-snippy/lib/ConsecutiveLoopsVerifierPass.cpp
@@ -72,11 +72,12 @@ bool ConsecutiveLoopsVerifier::runOnMachineFunction(MachineFunction &MF) {
   auto &GC = getAnalysis<GeneratorContextWrapper>().getContext();
   auto &Ctx = GC.getLLVMState().getCtx();
   auto &ConsLoops = GC.getConsecutiveLoops();
-  auto NConsLoops = GC.getGenSettings().Cfg.Branches.NConsecutiveLoops;
-  if (NConsLoops == 0) {
+  auto &BranchSettings = GC.getGenSettings().Cfg.Branches;
+  auto AnyConsLoops = BranchSettings.anyConsecutiveLoops();
+  if (!AnyConsLoops) {
     // Check that no consecutive loops were generated if they were not requested
     if (ConsLoops.size() != 0)
-      fatal(Ctx, "CFG verification",
+      fatal(Ctx, PASS_DESC,
             "No consecutive loops expected to be generated, generated " +
                 Twine(ConsLoops.size()));
     return false;
@@ -85,13 +86,29 @@ bool ConsecutiveLoopsVerifier::runOnMachineFunction(MachineFunction &MF) {
   auto &MLI = getAnalysis<MachineLoopInfo>();
   // Check max depth == 1
   if (any_of(MF, [&MLI](auto &MBB) { return MLI.getLoopDepth(&MBB) > 1; }))
-    fatal(Ctx, "CFG verification",
+    fatal(Ctx, PASS_DESC,
           "loop depth > 1 is not allowed with consecutive loops");
 
-  // Check that number of loops in pack satisfies requirements
-  if (any_of(make_second_range(ConsLoops),
-             [NConsLoops](auto &Cons) { return Cons.size() > NConsLoops; }))
-    fatal(Ctx, "CFG verification", "too many consecutive loop were generated");
+  if (!BranchSettings.onlyConsecutiveLoops()) {
+    // Check that number of loops in pack satisfies requirements
+    auto NConsLoops = BranchSettings.getNConsecutiveLoops();
+    if (any_of(make_second_range(ConsLoops),
+               [NConsLoops](auto &Cons) { return Cons.size() > NConsLoops; }))
+      fatal(Ctx, PASS_DESC, "too many consecutive loop were generated");
+  } else {
+    // Check that all blocks are consectuve loops
+    auto NBBs = MF.size();
+    if (ConsLoops.size() != 1)
+      fatal(Ctx, PASS_DESC, "only consecutive loops chain expected");
+    auto It = ConsLoops.find(0u);
+    if (It == ConsLoops.end())
+      fatal(Ctx, PASS_DESC,
+            "the first BB expected to be the first consecutive loop");
+    // NBBs == (NConsecutiveLoops + TerminatingBlock + FirstConsecutiveLoop +
+    // CommonPreheader)
+    if (It->second.size() != NBBs - 3)
+      fatal(Ctx, PASS_DESC, "all blocks expeted to be consecutive loops");
+  }
 
   // Check that loops oredred sequentially, i.e. N, N+1, N+2, N+3 ...
   if (any_of(ConsLoops, [](auto &CLEntry) {
@@ -99,7 +116,7 @@ bool ConsecutiveLoopsVerifier::runOnMachineFunction(MachineFunction &MF) {
                                     CLEntry.first + CLEntry.second.size());
         return !equal(CLEntry.second, BBNums);
       }))
-    fatal(Ctx, "CFG verification", "loops order is not sequential");
+    fatal(Ctx, PASS_DESC, "loops order is not sequential");
 
   // Check that loops are really generated consecutively
   if (any_of(make_second_range(ConsLoops), [&](auto &Cons) {
@@ -111,7 +128,7 @@ bool ConsecutiveLoopsVerifier::runOnMachineFunction(MachineFunction &MF) {
                  !MLI.isLoopHeader(ThisLoopHeader->getPrevNode());
         });
       }))
-    fatal(Ctx, "CFG verification", "loops are not consecutive");
+    fatal(Ctx, PASS_DESC, "loops are not consecutive");
 
   // Check that there is no first loop in consecutive loops set
   if (any_of(make_first_range(ConsLoops), [&ConsLoops](auto FirstLoop) {
@@ -119,7 +136,7 @@ bool ConsecutiveLoopsVerifier::runOnMachineFunction(MachineFunction &MF) {
           return Cons.count(FirstLoop);
         });
       }))
-    fatal(Ctx, "CFG verification",
+    fatal(Ctx, PASS_DESC,
           "first consecutive loop found in other cosecutive loops set");
 
   return false;
