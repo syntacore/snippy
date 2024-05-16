6988b6bcafa1cfc0b2753f45ed1cbd9d5b97c03a
Alexander Romanov <alexander.romanov@syntacore.com>
2024-05-16 11:02:18 +0000

    [snippy] reinvent generation requests Currently generation requests in llvm-snippy are an overengineering. We have a very complex classes with many responsibilities. The hierarchy is very hard to extend (And we want be able to add new policies). We also don't have clear generation modes (They are implicit and information is being passed around in a very unclear ways). This commit introduces new GenerationPolicy hierarchy (Without intrusive inheritance) that is very easy to extend and add new Policies to. Generation requests were hugely simplified. All of that caused some changes in generation process. In this commit new generic generation process is being introduced, so that adding new policies does not require changes in code generator. All of that allows for better and easier extensibility of the project in the future.

--- llvm/tools/llvm-snippy/include/snippy/Generator/Policy.h
+++ llvm/tools/llvm-snippy/include/snippy/Generator/Policy.h
@@ -5,76 +5,351 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+/// \file Header containing Generation Policy "concept"
+///
+/// The primary class defined in this header is GenPolicy, which serves as a
+/// holder for any class or struct that meets certain requirements. We will
+/// refer to these qualifying classes as "Policies." To be considered a Policy,
+/// a class must implement specific free functions. For an abstract class
+/// named "MyPolicy," the signatures of these functions are as follows:
+///
+/// 1.
+/// std::optional<InstructionRequest> next(MyPolicy &);
+///
+/// The "next" function serves as a mechanism by which code generation functions
+/// request the next InstructionRequest to fulfill. It can return std::nullopt
+/// if the MyPolicy class can only generate a limited number of requests. A
+/// return of std::nullopt would indicate the end of the instruction group.
+///
+/// 2.
+/// void initialize(MyPolicy &, InstructionGenerationContext &,
+///                const RequestLimit &);
+///
+/// The "initialize" function acts as a hook into the generation process, called
+/// before generating any instructions in the group. This function allows
+/// MyPolicy to perform any necessary preparations before generating the group.
+/// These preparations might include initializing registers or memory, setting
+/// certain control and status registers (CSRs), etc. The function takes the
+/// current generation context as an argument so that MyPolicy can prepare for
+/// generation using that information (e.g., which instructions were already
+/// generated in the basic block or which CSRs have specific values). MyPolicy
+/// might only be able to generate a finite number of InstructionRequests. For
+/// this reason, the "initialize" function takes a RequestLimit argument to
+/// determine how many instructions (requested size or anything else) are
+/// planned to be generated using that policy and prepare the required number of
+/// InstructionRequests.
+///
+/// 3.
+/// void finalize(MyPolicy &Policy, InstructionGenerationContext &);
+///
+/// The "finalize" function acts as the counterpart to "initialize" and is
+/// called after generating all the instructions in the group. MyPolicy can
+/// perform any required finalization actions in this function.
+///
+/// 4.
+/// bool isInseparableBundle(MyPolicy &);
+///
+/// The "isInseparableBundle" function indicates whether generated
+/// instructions must be executed one by one (on the simulator) instead of
+/// executing the entire group when generation is complete. This functionality
+/// is useful for SelfCheck and other tracking modes.
+///
+/// 5.
+/// void print(MyPolicy &, raw_ostream &);
+///
+/// The "print" function is used solely for debugging purposes. It prints any
+/// necessary information about MyPolicy.
+//===----------------------------------------------------------------------===//
 
 #pragma once
 
+#include "snippy/Config/ImmediateHistogram.h"
 #include "snippy/Config/OpcodeHistogram.h"
+#include "snippy/Generator/GenerationLimit.h"
+#include "snippy/Generator/RegisterPool.h"
 #include "snippy/Support/OpcodeGenerator.h"
+
+#include <functional>
+#include <memory>
 
 namespace llvm {
 namespace snippy {
 
 class GeneratorContext;
+namespace planning {
 
-struct IGenPolicy {
-  virtual unsigned genNextOpc() = 0;
-  virtual void print(raw_ostream &OS) const = 0;
-#if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
-  LLVM_DUMP_METHOD void dump() const { print(dbgs()); }
-#endif
-  virtual ~IGenPolicy() = default;
+// Helper class to keep additional information about the operand: register
+// number that has been somehow selected before instruction generation,
+// immediate operand range and so on.
+class PreselectedOpInfo {
+  using EmptyTy = std::monostate;
+  using RegTy = llvm::Register;
+  using ImmTy = StridedImmediate;
+  using TiedTy = int;
+  std::variant<EmptyTy, RegTy, ImmTy, TiedTy> Value;
+
+  unsigned Flags = 0;
+
+public:
+  PreselectedOpInfo(llvm::Register R) : Value(R) {}
+  PreselectedOpInfo(StridedImmediate Imm) : Value(Imm) {}
+  PreselectedOpInfo() = default;
+  bool isReg() const { return std::holds_alternative<RegTy>(Value); }
+  bool isImm() const { return std::holds_alternative<ImmTy>(Value); }
+  bool isUnset() const { return std::holds_alternative<EmptyTy>(Value); }
+  bool isTiedTo() const { return std::holds_alternative<TiedTy>(Value); }
+
+  unsigned getFlags() const { return Flags; }
+  StridedImmediate getImm() const {
+    assert(isImm());
+    return std::get<ImmTy>(Value);
+  }
+  llvm::Register getReg() const {
+    assert(isReg());
+    return std::get<RegTy>(Value);
+  }
+  llvm::Register getTiedTo() const {
+    assert(isTiedTo());
+    return std::get<TiedTy>(Value);
+  }
+
+  void setFlags(unsigned F) { Flags = F; }
+  void setTiedTo(int OpIdx) {
+    assert(isUnset());
+    Value = OpIdx;
+  }
 };
 
-using GenPolicy = std::unique_ptr<IGenPolicy>;
+struct InstructionGenerationContext final {
+  MachineBasicBlock &MBB;
+  MachineBasicBlock::iterator Ins;
+  RegPoolWrapper &RP;
+  GeneratorContext &GC;
+};
 
-class DefaultGenPolicy final : public IGenPolicy {
+struct InstructionRequest final {
+  unsigned Opcode;
+  std::vector<PreselectedOpInfo> Preselected;
+};
+
+namespace detail {
+struct EmptyFinalizeMixin {
+  void finalize(InstructionGenerationContext &) {}
+};
+} // namespace detail
+
+class DefaultGenPolicy final : public detail::EmptyFinalizeMixin {
   OpcGenHolder OpcGen;
 
 public:
+  DefaultGenPolicy(const DefaultGenPolicy &Other)
+      : OpcGen(Other.OpcGen->copy()) {}
+
+  DefaultGenPolicy(DefaultGenPolicy &&) = default;
+
+  DefaultGenPolicy &operator=(const DefaultGenPolicy &Other) {
+    DefaultGenPolicy Tmp = Other;
+    std::swap(*this, Tmp);
+    return *this;
+  }
+
+  DefaultGenPolicy &operator=(DefaultGenPolicy &&) = default;
+
   DefaultGenPolicy(const GeneratorContext &SGCtx,
                    std::function<bool(unsigned)> Filter,
                    bool MustHavePrimaryInstrs,
                    ArrayRef<OpcodeHistogramEntry> Overrides);
 
-  unsigned genNextOpc() override { return OpcGen->generate(); }
-
-  void print(raw_ostream &OS) const override {
-    OS << "Default Generation Policy\n";
+  std::optional<InstructionRequest> next() const {
+    return InstructionRequest{OpcGen->generate(), {}};
   }
+  void initialize(InstructionGenerationContext &InstrGenCtx,
+                  const RequestLimit &Limit) const {}
+
+  bool isInseparableBundle() const { return false; }
+
+  void print(raw_ostream &OS) const { OS << "Default Generation Policy\n"; }
 };
 
-class BurstGenPolicy final : public IGenPolicy {
+class BurstGenPolicy final : public detail::EmptyFinalizeMixin {
   std::vector<unsigned> Opcodes;
   std::discrete_distribution<size_t> Dist;
+  std::vector<InstructionRequest> Instructions;
+  unsigned Idx = 0;
+  unsigned genOpc() { return Opcodes.at(Dist(RandEngine::engine())); }
 
 public:
-  BurstGenPolicy(const GeneratorContext &SGCtx, unsigned BurstGroupID);
-
-  unsigned genNextOpc() override {
-    return Opcodes.at(Dist(RandEngine::engine()));
+  std::optional<InstructionRequest> next() {
+    if (Idx < Instructions.size())
+      return Instructions[Idx++];
+    return std::nullopt;
   }
 
-  void print(raw_ostream &OS) const override {
+  BurstGenPolicy(const GeneratorContext &SGCtx, unsigned BurstGroupID);
+
+  void initialize(InstructionGenerationContext &InstrGenCtx,
+                  const RequestLimit &Limit);
+
+  void print(raw_ostream &OS) const {
     OS << "Burst Generation Policy: { ";
     for (auto [Opc, Prob] : zip(Opcodes, Dist.probabilities())) {
       OS << Opc << "(" << Prob << ") ";
     }
-    OS << "}\n";
+    OS << " }\n";
   }
+
+  bool isInseparableBundle() const { return true; }
 };
 
-class FinalInstPolicy final : public IGenPolicy {
+class FinalInstPolicy final : public detail::EmptyFinalizeMixin {
   unsigned Opcode;
 
 public:
   FinalInstPolicy(unsigned Opc) : Opcode(Opc) {}
 
-  unsigned genNextOpc() override { return Opcode; }
+  bool isInseparableBundle() const { return false; }
 
-  void print(raw_ostream &OS) const override {
+  std::optional<InstructionRequest> next() const {
+    return InstructionRequest{Opcode, {}};
+  }
+
+  void initialize(InstructionGenerationContext &InstrGenCtx,
+                  const RequestLimit &Limit) const {}
+
+  void print(raw_ostream &OS) const {
     OS << "Final Instruction Policy (" << Opcode << ")\n";
   }
 };
 
+template <typename PolicyTy>
+std::optional<InstructionRequest> next(PolicyTy &Policy) {
+  return Policy.next();
+}
+
+template <typename PolicyTy>
+void initialize(PolicyTy &Policy, InstructionGenerationContext &InstrGenCtx,
+                const RequestLimit &Limit) {
+  Policy.initialize(InstrGenCtx, Limit);
+}
+
+template <typename PolicyTy>
+void finalize(PolicyTy &Policy, InstructionGenerationContext &InstrGenCtx) {
+  Policy.finalize(InstrGenCtx);
+}
+
+template <typename PolicyTy> bool isInseparableBundle(PolicyTy &Policy) {
+  return Policy.isInseparableBundle();
+}
+
+template <typename PolicyTy> void print(PolicyTy &Pol, raw_ostream &OS) {
+  Pol.print(OS);
+}
+
+///\class GenPolicy
+///
+/// A holder for any PolicyTy that meets the requirements described above.
+/// Movable, copyable, has value semantics.
+class GenPolicy final {
+  struct Concept {
+    virtual ~Concept() = default;
+    virtual std::unique_ptr<Concept> copy() const = 0;
+    virtual std::optional<InstructionRequest> next() = 0;
+    virtual void initialize(InstructionGenerationContext &InstrGenCtx,
+                            const RequestLimit &Limit) = 0;
+    virtual void finalize(InstructionGenerationContext &InstrGenCtx) = 0;
+    virtual bool isInseparableBundle() const = 0;
+    virtual void print(raw_ostream &OS) const = 0;
+  };
+
+  template <typename PolicyTy> class Model final : public Concept {
+    PolicyTy Data;
+    static_assert(std::is_copy_constructible_v<std::decay_t<PolicyTy>>,
+                  "Policy should be copy constructible.");
+
+  public:
+    Model(PolicyTy Pol) : Data(std::move(Pol)) {}
+
+    std::unique_ptr<Concept> copy() const override {
+      return std::make_unique<Model>(Data);
+    }
+
+    std::optional<InstructionRequest> next() override {
+      return planning::next(Data);
+    }
+
+    void initialize(InstructionGenerationContext &InstrGenCtx,
+                    const RequestLimit &Limit) override {
+      planning::initialize(Data, InstrGenCtx, Limit);
+    }
+
+    void finalize(InstructionGenerationContext &InstrGenCtx) override {
+      planning::finalize(Data, InstrGenCtx);
+    }
+
+    bool isInseparableBundle() const override {
+      return planning::isInseparableBundle(Data);
+    }
+
+    void print(raw_ostream &OS) const override { planning::print(Data, OS); }
+  };
+
+  template <typename PolicyTy> Model(PolicyTy) -> Model<PolicyTy>;
+
+  std::unique_ptr<Concept> Impl;
+
+public:
+  template <typename PolicyTy>
+  GenPolicy(PolicyTy Pol)
+      : Impl(std::make_unique<Model<PolicyTy>>(std::move(Pol))) {}
+
+  GenPolicy(const GenPolicy &Other) : Impl(Other.Impl->copy()) {}
+
+  GenPolicy(GenPolicy &&Other) = default;
+
+  GenPolicy &operator=(const GenPolicy &Other) {
+    GenPolicy Tmp = Other;
+    std::swap(*this, Tmp);
+    return *this;
+  }
+
+  GenPolicy &operator=(GenPolicy &&Other) = default;
+
+  friend std::optional<InstructionRequest> next(const GenPolicy &Pol);
+
+  friend void initialize(const GenPolicy &Pol,
+                         InstructionGenerationContext &InstrGenCtx,
+                         const RequestLimit &Limit);
+
+  friend void finalize(const GenPolicy &Pol,
+                       InstructionGenerationContext &InstrGenCtx);
+
+  friend bool isInseparableBundle(const GenPolicy &Pol);
+
+  friend void print(const GenPolicy &Pol, raw_ostream &OS);
+};
+
+inline std::optional<InstructionRequest> next(const GenPolicy &Pol) {
+  return Pol.Impl->next();
+}
+
+inline void initialize(const GenPolicy &Pol,
+                       InstructionGenerationContext &InstrGenCtx,
+                       const RequestLimit &Limit) {
+  return Pol.Impl->initialize(InstrGenCtx, Limit);
+}
+
+inline void finalize(const GenPolicy &Pol,
+                     InstructionGenerationContext &InstrGenCtx) {
+  return Pol.Impl->finalize(InstrGenCtx);
+}
+inline bool isInseparableBundle(const GenPolicy &Pol) {
+  return Pol.Impl->isInseparableBundle();
+}
+
+inline void print(const GenPolicy &Pol, raw_ostream &OS) {
+  Pol.Impl->print(OS);
+}
+
+} // namespace planning
 } // namespace snippy
 } // namespace llvm
