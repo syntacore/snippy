b9a6fd1320273dc274576f659a4ec7b74410318c
vb-sc <vladislav.belov@syntacore.com>
2025-04-15 19:07:16 +0000

    [snippy] Drop export-gv option

--- llvm/tools/llvm-snippy/lib/Generator/MemoryManager.cpp
+++ llvm/tools/llvm-snippy/lib/Generator/MemoryManager.cpp
@@ -31,86 +31,8 @@ namespace llvm::snippy {
 
 namespace {
 
-void fillProgSectionInfo(Linker &L, const GlobalCodeFlowInfo &GCFI,
-                         MemoryConfig &Config) {
-  for (auto &ExecSection : GCFI.ExecutionPath) {
-    Config.ProgSections.emplace_back(
-        ExecSection.OutputSection.Desc.VMA, ExecSection.OutputSection.Desc.Size,
-        L.getMangledName(ExecSection.OutputSection.Name));
-    LLVM_DEBUG(
-        dbgs() << "ProgramStart: " << Config.ProgSections.back().Start << "\n";
-        dbgs() << "ProgramSize: " << Config.ProgSections.back().Size << "\n");
-  }
 
-  if (Config.ProgSections.empty())
-    snippy::fatal("Incorrect list of sections: no used RX sections found");
-}
-
-MemorySectionConfig getRamInfo(const Linker &L) {
-  auto IsRW = [](const Linker::SectionEntry &S) {
-    auto &M = S.OutputSection.Desc.M;
-    return M.R() && M.W() && !M.X();
-  };
-  const auto &Sections = L.sections();
-  auto RWSectionIt = std::find_if(Sections.begin(), Sections.end(), IsRW);
-  assert(RWSectionIt != Sections.end());
-
-  auto RWSectionLast =
-      std::find_if(Sections.rbegin(), Sections.rend(), IsRW).base();
-
-  if (std::any_of(RWSectionIt, RWSectionLast, std::not_fn(IsRW)) ||
-      (RWSectionIt != Sections.begin() && RWSectionLast != Sections.end()))
-    snippy::fatal("Incorrect list of sections: all RW sections must go "
-                  "either before all other sections or after them all");
-
-  const auto &OutputSectionDesc = RWSectionIt->OutputSection.Desc;
-  auto RamVMABegin = OutputSectionDesc.VMA;
-  auto MaxRamVMAEnd = RamVMABegin + OutputSectionDesc.Size;
-
-  for (const auto &S : llvm::make_range(++RWSectionIt, RWSectionLast)) {
-    RamVMABegin = std::min(RamVMABegin, S.OutputSection.Desc.VMA);
-    MaxRamVMAEnd = std::max(MaxRamVMAEnd, S.OutputSection.Desc.VMA +
-                                              S.OutputSection.Desc.Size);
-  }
-  assert(MaxRamVMAEnd >= RamVMABegin);
-  return {RamVMABegin, MaxRamVMAEnd - RamVMABegin, ""};
-}
-
-MemorySectionConfig getRomInfo(const Linker &L, MemAddr ProgSectionStart) {
-  MemAddr RomStart;
-  MemAddr RomSize;
-  std::string RomSectionName;
-  if (L.sections().hasOutputSectionFor(".rodata")) {
-    const auto &ROMSection = L.sections().getOutputSectionFor(".rodata");
-    const auto &ROMSectionDesc = ROMSection.Desc;
-    RomStart = ROMSectionDesc.VMA;
-    RomSize = ROMSectionDesc.Size;
-    RomSectionName = L.getMangledName(ROMSection.Name);
-  } else {
-    RomStart = ProgSectionStart;
-    RomSize = 0;
-  }
-
-  LLVM_DEBUG(dbgs() << "ROM Start: " << RomStart << "\n";
-             dbgs() << "ROM Size: " << RomSize << "\n";
-             dbgs() << "ROM Section name: " << RomSectionName << "\n");
-
-  return {RomStart, RomSize, RomSectionName};
-}
 
 } // namespace
-
-MemoryConfig MemoryConfig::getMemoryConfig(Linker &L,
-                                           GlobalCodeFlowInfo &GCFI) {
-  MemoryConfig Config{};
-  // get RX sections info...
-  fillProgSectionInfo(L, GCFI, Config);
-  // get R sections info...
-  Config.Rom = getRomInfo(L, Config.ProgSections.front().Start);
-  // get RW sections info...
-  Config.Ram = getRamInfo(L);
-
-  return Config;
-}
 
 } // namespace llvm::snippy
