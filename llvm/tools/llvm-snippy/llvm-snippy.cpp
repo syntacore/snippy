51b6cee036889b27424905a5e6f17b755503db78
Sergei Zimmerman <sergei.zimmerman@syntacore.com>
2025-05-07 12:48:06 +0000

    [snippy] Generate driver options with tablegen

--- llvm/tools/llvm-snippy/llvm-snippy.cpp
+++ llvm/tools/llvm-snippy/llvm-snippy.cpp
@@ -45,24 +45,12 @@ namespace snippy {
 
 extern cl::OptionCategory Options;
 
-// Unlike clang, march here is not something like rv64gc (provided by attrs and
-// CPU). Instead it is something like target (try riscv64-linux-gnu).
-static snippy::opt<std::string>
-    MArch("march", cl::desc("target architecture"),
-          cl::value_desc("one of suppported Archs: RISCV, etc.."),
-          cl::cat(Options), cl::init(""));
-
-static snippy::opt<std::string>
-    CpuName("mcpu", cl::desc("cpu name to use, leave empty to autodetect"),
-            cl::cat(Options), cl::init(""));
-
-static snippy::opt<std::string> MAttr(
-    "mattr", cl::desc("comma-separated list of target architecture features"),
-    cl::value_desc("+feature1,-feature2,..."), cl::cat(Options), cl::init(""));
-
-static snippy::opt<std::string> ABI("mabi",
-                                    cl::desc("custom abi for output elf-files"),
-                                    cl::cat(Options), cl::init(""));
+// TODO: All options should be read into a struct all passed around through
+// config. There are some options that are necessary to bootstrap the config so
+// they'd have to be left in the driver.
+#define GEN_SNIPPY_OPTIONS_DEF
+#include "SnippyDriverOptions.inc"
+#undef GEN_SNIPPY_OPTIONS_DEF
 
 // YAML file with memory layout and histogram
 static cl::opt<std::string> LayoutFile(cl::Positional, cl::desc("<layout>"),
@@ -71,15 +59,6 @@ static cl::opt<std::string> LayoutFile(cl::Positional, cl::desc("<layout>"),
 static cl::list<std::string> AdditionalLayoutFiles(cl::Positional,
                                                    cl::desc("<sub-layouts>..."),
                                                    cl::cat(Options));
-
-static snippy::opt_list<std::string> LayoutIncludeDirectories(
-    "layout-include-dir", cl::CommaSeparated,
-    cl::desc("extra directory where to look for include files"),
-    cl::cat(Options));
-
-static snippy::opt<bool>
-    ListOpcodeNames("list-opcode-names",
-                    cl::desc("list available opcode names"), cl::cat(Options));
 
 std::optional<unsigned> getExpectedNumInstrs(StringRef NumAsString) {
   if (NumAsString == "all")
@@ -92,56 +71,12 @@ std::optional<unsigned> getExpectedNumInstrs(StringRef NumAsString) {
   return Value;
 }
 
-static snippy::opt<bool> DumpMF(
-    "dump-mf",
-    cl::desc("[Debug] dump final generated machine function in Machine IR"),
-    cl::cat(Options));
-
-static snippy::opt<bool> DumpMI(
-    "dump-mi",
-    cl::desc("[Debug] dump generated machine instructions in Machine IR"),
-    cl::cat(Options));
-
-static snippy::opt<bool> DumpCFG("dump-cfg", cl::desc("Dump generated CFG"),
-                                 cl::cat(Options));
-
 static snippy::opt<bool> ViewCFG("view-cfg", cl::desc("View generated CFG"),
                                  cl::cat(Options),
                                  cl::callback([](const bool &V) {
                                    if (V)
                                      DumpCFG = true;
                                  }));
-
-static snippy::opt<std::string>
-    Seed("seed",
-         cl::desc("seed for instruction generation. If the option is not used "
-                  "or its value is not set, seed will be generated randomly."),
-         cl::cat(Options));
-
-static snippy::opt<std::string>
-    OutputFileBasename("o", cl::desc("Override output file base name"),
-                       cl::value_desc("filename"), cl::cat(Options));
-
-static snippy::opt<std::string>
-    GeneratorPluginFile("generator-plugin",
-                        cl::desc("Plugin for custom instruction generation."
-                                 "Use =None to generate instructions "
-                                 "with build-in histogram."
-                                 "(=None - default value)"),
-                        cl::value_desc("filename"), cl::cat(Options),
-                        cl::init("None"));
-
-static snippy::opt<std::string> GeneratorPluginParserFile(
-    "plugin-info-file",
-    cl::desc("File with info to parse with plugin generator. "
-             "Use =None to parse histogram with build-in "
-             "Snippy pareser."),
-    cl::value_desc("filename"), cl::cat(Options), cl::init("None"));
-
-static snippy::opt<bool>
-    DumpLayout("dump-layout",
-               cl::desc("Dump the whole snippy configuration YAML"),
-               cl::init(false), cl::cat(Options));
 
 static snippy::opt<bool> Verbose("verbose", cl::desc("Show verbose output."),
                                  cl::init(false), cl::cat(Options),
@@ -152,14 +87,6 @@ static snippy::opt<bool> Verbose("verbose", cl::desc("Show verbose output."),
                                      DumpMI = true;
                                    }
                                  }));
-
-static snippy::opt<bool> DumpOptions("dump-options",
-                                     cl::desc("Dump snippy options' values"),
-                                     cl::cat(Options), cl::init(false));
-
-static snippy::opt<bool> DumpPreprocessedConfig(
-    "E", cl::desc("Dump snippy config after preprocessing it"), cl::init(false),
-    cl::cat(Options));
 
 // Used to construct search paths for dynamically-loaded plugins
 static std::string ARGV0;
---
+++ llvm/tools/llvm-snippy/utils/TableGen/TableGen.cpp
@@ -0,0 +1,62 @@
+//===- TableGen.cpp -------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "SnippyOptions.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/TableGen/Main.h"
+#include "llvm/TableGen/Record.h"
+
+namespace llvm {
+namespace snippy {
+
+enum ActionType {
+  PrintRecords,
+  DumpJSON,
+  GenOptions,
+};
+
+static cl::opt<ActionType>
+    Action(cl::desc("Action to perform:"),
+           cl::values(clEnumValN(PrintRecords, "print-records",
+                                 "Print all records to stdout (default)"),
+                      clEnumValN(DumpJSON, "dump-json",
+                                 "Dump all records as machine-readable JSON"),
+                      clEnumValN(GenOptions, "gen-options",
+                                 "Generate option definitions")));
+
+static bool snippyTableGenMain(raw_ostream &OS, RecordKeeper &Records) {
+  switch (Action) {
+  case PrintRecords:
+    OS << Records; // No argument, dump all contents
+    break;
+  case DumpJSON:
+    EmitJSON(Records, OS);
+    break;
+  case GenOptions:
+    emitSnippyOptions(OS, Records);
+    break;
+  }
+
+  return false;
+}
+
+} // namespace snippy
+} // namespace llvm
+
+int main(int argc, char **argv) {
+  using namespace llvm;
+  using namespace llvm::snippy;
+  sys::PrintStackTraceOnErrorSignal(argv[0]);
+  PrettyStackTraceProgram StackTrace(argc, argv);
+  cl::ParseCommandLineOptions(argc, argv);
+  llvm_shutdown_obj Shutdown;
+  return TableGenMain(argv[0], &snippyTableGenMain);
+}
