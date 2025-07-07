b2ad3f229626734c728a2a2b77bb7a598fbe3f4d
Sergei Zimmerman <sergei.zimmerman@syntacore.com>
2025-07-07 14:41:06 +0000

    [snippy] Split Valuegram operand value source into an interface + implementation
    This (NFC) patch untangles the logic of value selection, and actually
    writing to registers. These two things are independent and one might
    want to substitute multiple value sources to this Policy.
    
    Some bug fixes along the way. Specially this:
    
    ```{.cpp}
    llvm::copy_if(OpsRegs, std::back_inserter(Registers),
                  [Registers /* Notice the capture by value here*/]
                  (const auto &Operand) {
                    if (llvm::is_contained(Registers, Operand))
                      return false;
                    return true;
                  });
    ```
    
    Has been replaced with the correct implementation:
    
    ```{.cpp}
    for (auto Operand : OpsRegs) {
      if (!llvm::is_contained(Registers, Operand))
        Registers.push_back(Operand);
    }
    ```
    
    Quadratic copy because PreselctedOpInfo are unordered and
    defining an order might be somewhat tricky. This vector is always
    very small, so the penalty of using an O(n^2) is negligible.

---
+++ llvm/tools/llvm-snippy/lib/Generator/OperandsReinitializationValueSource.cpp
@@ -0,0 +1,91 @@
+//===-- OperandsReinitializationValueSource.cpp -----------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "snippy/Generator/GenerationUtils.h"
+#include "snippy/Generator/OperandsReinitialiazationPolicy.h"
+#include "snippy/Generator/SimulatorContext.h"
+#include "snippy/Target/Target.h"
+
+namespace llvm {
+namespace snippy {
+
+namespace planning {
+
+// FIXME: Layer violation. This code should be target-independent.
+static std::string getRegistersPrefix(RegStorageType Storage) {
+  switch (Storage) {
+  case RegStorageType::XReg:
+    return "X";
+  case RegStorageType::FReg:
+    return "F";
+  case RegStorageType::VReg:
+    return "V";
+  }
+  llvm_unreachable("Unknown storage type");
+}
+
+Expected<std::optional<APInt>>
+OperandsReinitializationValuegramSource::sampleRegisterOperandValueForInstr(
+    MCOperand Operand, unsigned OperandIndex,
+    [[maybe_unused]] const MCInstrDesc &InstrDesc,
+    InstructionGenerationContext &IGC) {
+  const auto &RegsHistograms = getRegHistograms();
+  const auto &ClassHistograms = RegsHistograms.Histograms.ClassHistograms;
+  using ReturnType = Expected<std::optional<APInt>>;
+
+  // Can only handle register operands. 'Fixing/freezing' immediate values
+  // can be a valid use-case for some operand sources. Like direct selection
+  // of floating-point rounding modes.
+  if (!Operand.isReg())
+    // Being explicit about return type because CTAD is broken for some older
+    // toolchains.
+    return ReturnType(std::nullopt);
+
+  const auto &ProgCtx = IGC.ProgCtx;
+  const auto &State = ProgCtx.getLLVMState();
+  const auto &Tgt = State.getSnippyTarget();
+  Register Reg = Operand.getReg();
+  auto Prefix = getRegistersPrefix(Tgt.regToStorage(Reg));
+
+  // FIXME: Layer violation. This code doesn't have to know about arch-specific
+  // register prefix names.
+  auto It = std::find_if(ClassHistograms.begin(), ClassHistograms.end(),
+                         [Prefix](const RegisterClassHistogram &CH) {
+                           return CH.RegType == Prefix;
+                         });
+
+  auto MCReg = Reg.asMCReg();
+  auto BitWidth = Tgt.getRegBitWidth(MCReg, IGC);
+
+  // This means that the histogram contains the required type of registers.
+  // We generate the value from the "histograms".
+  if (It != ClassHistograms.end()) {
+    const auto &Hist = *It;
+    const auto &Valuegram = Hist.TheValuegram;
+    std::discrete_distribution<size_t> Dist(Valuegram.weights_begin(),
+                                            Valuegram.weights_end());
+    return sampleValuegramForOneReg(Valuegram, Prefix, BitWidth, Dist);
+  }
+
+  // Otherwise, we generate the value from the "registers".
+  auto NumReg = Tgt.regToIndex(Reg);
+  auto NumRegs = Tgt.getNumRegs(Tgt.regToStorage(Reg), IGC.getSubtargetImpl());
+  ScratchAPInts.resize(NumRegs);
+  getFixedRegisterValues(RegsHistograms, NumRegs, Prefix, BitWidth,
+                         ScratchAPInts);
+  if (ScratchAPInts.size() <= NumReg)
+    return makeFailure(Errc::InvalidConfiguration, "Valuegram error",
+                       "No values for " + Prefix + std::to_string(NumReg) +
+                           " registers");
+
+  return ScratchAPInts[NumReg];
+}
+
+} // namespace planning
+} // namespace snippy
+} // namespace llvm
