9fc820ef90cf9cad7562e2814d68b971a3e275c9
Ilya Gavrilin <ilya.gavrilin@syntacore.com>
2026-03-25 13:11:21 +0000

    [snippy] Remove register-plugin interface
    This patch removes register-plugin interface. Also,
    loop during operand generation removed too

--- llvm/tools/llvm-snippy/include/snippy/Generator/RegisterGenerator.h
+++ llvm/tools/llvm-snippy/include/snippy/Generator/RegisterGenerator.h
@@ -14,7 +14,6 @@
 #pragma once
 
 #include "snippy/GeneratorUtils/RegisterPool.h"
-#include "snippy/Plugins/RegisterPluginCInterface.h"
 #include "snippy/Target/Target.h"
 
 #include "llvm/CodeGen/Register.h"
@@ -27,116 +26,25 @@ namespace snippy {
 
 class GeneratorContext;
 
-// Definition of the ::RegVerifierHandle from the RegisterPluginCInterface.h
-// Includes all the infromation needed to determine
-//  whether the register index is valid for the current operand
-struct RegVerifierHandle {
-  const MCRegisterClass *RC = nullptr;
-  const MCRegisterInfo *RI = nullptr;
-  const MachineBasicBlock *MBB = nullptr;
-  const RegPoolWrapper *RP = nullptr;
-  std::vector<Register> Exclude;
-  std::vector<Register> Include;
-  AccessMaskBit Mask;
-
-public:
-  static RegValidation
-  regIsValidExtern(unsigned RegIdx,
-                   const ::RegVerifierHandle *RegVerifierHandleObj);
-
-  void setState(const MCRegisterClass &RCIn, const MCRegisterInfo &RIIn,
-                const RegPoolWrapper &RPIn, const MachineBasicBlock &MBBIn,
-                ArrayRef<Register> ExcludeIn, ArrayRef<Register> IncludeIn,
-                AccessMaskBit MaskIn);
-
-  bool regIsValid(unsigned RegIdx) const;
-
-  void checkState() const;
-
-  unsigned getMaxRegIndex() const;
-
-  void clear();
-};
-
-// Definition of the ::RegTranslatorHandle from the RegisterPluginCInterface.h
-// Includes all the information needed to translate string to register index
-//  for the current operand
-class RegTranslatorHandle {
-  const MCRegisterClass *RC = nullptr;
-  const MCRegisterInfo *RI = nullptr;
-
-public:
-  static unsigned
-  getRegFromStr(const char *RegStr,
-                const ::RegTranslatorHandle *RegTranslatorHandleObj);
-
-  void setState(const MCRegisterClass &RCIn, const MCRegisterInfo &RIIn);
-
-  void checkState() const;
-
-  void clear();
-};
-
 class RegisterGenerator final {
-  const RegPluginFunctionsTable *DLTable = nullptr;
-  RegVerifierHandle RegVerifier;
-  RegTranslatorHandle RegTranslator;
   bool RequiresRollBack = false;
 
-  void loadPluginDL(const std::string &PluginLibName);
-
-  void setRegPluginInfo(const std::string &RegPluginInfoFile) const;
-
-  // Calls interface function generate()
-  //  with the corresponding  SnippyRegContext.
-  // After this call RegVerifier and RegTranslator are zero-initialized.
-  Expected<std::optional<Register>>
-  generateWithPlugin(const MCRegisterClass &RC, const MCRegisterInfo &RI,
-                     const RegPoolWrapper &RP, const MachineBasicBlock &MBB,
-                     ArrayRef<Register> Exclude, ArrayRef<Register> Include,
-                     AccessMaskBit Mask);
-
-  Expected<std::optional<Register>>
+  Expected<Register>
   generateRandom(const SnippyTarget &SnippyTgt, const MCRegisterClass &RC,
                  const MCRegisterInfo &RI, const RegPoolWrapper &RP,
                  const MachineBasicBlock &MBB, ArrayRef<Register> Exclude,
                  ArrayRef<Register> Include, AccessMaskBit Mask) const;
 
 public:
-  RegisterGenerator(const std::string &PluginLibName,
-                    const std::string &RegPluginInfoFile) {
-    if (!PluginLibName.empty())
-      loadPluginDL(PluginLibName);
-    if (!RegPluginInfoFile.empty())
-      setRegPluginInfo(RegPluginInfoFile);
-  }
-
-  bool regGenIsWithPlugin() const { return DLTable != nullptr; }
-
-  // This function passes SnippyRegContext to the plugin if it is enabled
-  void setRegContextForPlugin();
-
-  // Returns register either from (future)plugin or from random generator.
-  // std::nullopt is valid return value if we are using plugins
-  // TODO: remove this optional after removing plugins
-  Expected<std::optional<Register>>
+  // Returns register from random generator.
+  Expected<Register>
   generate(const MCRegisterClass &RC, unsigned OperandRegClassID,
            const MCRegisterInfo &RI, const RegPoolWrapper &RP,
            const MachineBasicBlock &MBB, const SnippyTarget &SnippyTgt,
            ArrayRef<Register> Exclude = {}, ArrayRef<Register> Include = {},
            AccessMaskBit Mask = AccessMaskBit::RW) {
     assert(!RequiresRollBack && "Can't generate without failure recovery");
-    assert(none_of(Include,
-                   [&RP, &MBB, Mask](unsigned Reg) {
-                     return RP.isReserved(Reg, MBB, Mask);
-                   }) &&
-           "Registers that have been reserved can't be included");
-    auto Result =
-        regGenIsWithPlugin()
-            ? generateWithPlugin(RC, RI, RP, MBB, Exclude, Include, Mask)
-            : generateRandom(SnippyTgt, RC, RI, RP, MBB, Exclude, Include,
-                             Mask);
-    return Result;
+    return generateRandom(SnippyTgt, RC, RI, RP, MBB, Exclude, Include, Mask);
   }
 };
 
