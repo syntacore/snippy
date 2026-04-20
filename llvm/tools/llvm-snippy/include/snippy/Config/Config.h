ae37c99a26eb9c82ac4daa7af623e41fe97e24ea
Dmitry Bushev <dmitry.bushev@syntacore.com>
2026-04-20 12:55:25 +0000

    [snippy] Allow redefinition of RA
    Added new option `redefine-ra` that allows to redefine register to
    be used as return address. It works similar to `redefine-sp`.

--- llvm/tools/llvm-snippy/include/snippy/Config/Config.h
+++ llvm/tools/llvm-snippy/include/snippy/Config/Config.h
@@ -50,6 +50,7 @@ public:
   // stack frame specific.
   std::string ABIName;
   MCRegister StackPointer;
+  MCRegister ReturnAddress;
   bool FollowTargetABI;
   std::vector<std::string> PreserveCallerSavedGroups;
   SmallVector<MCRegister> SpilledToStack;
