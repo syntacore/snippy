5af911edb0df5cb937c2298c654cd82ba5f18154
vb-sc <vladislav.belov@syntacore.com>
2026-05-07 14:44:56 +0000

    [snippy] implement random scheduling deoptimization This patch implements deoptimization of the generated snippet by reordering instructions. It is guaranteed that the final state of the register file and memory remains the same.
    The algorithm involves the following steps:
    1) A dependency graph of instructions is built based on RAW, WAR, and WAW dependencies.
    2) An arbitrary reverse post order (RPO) traversal of this graph is selected.
    3) The code is reordered according to the chosen approach.
    
    In practice, multiple different graphs must be constructed to preserve control flow as well as the observed behavior regarding instructions with non-trivial semantics (e.g., fence).

--- llvm/tools/llvm-snippy/lib/Target/X86/Target.cpp
+++ llvm/tools/llvm-snippy/lib/Target/X86/Target.cpp
@@ -409,6 +409,10 @@ public:
     reportUnimplementedError();
   }
 
+  bool mayBeScheduled(const MachineInstr &MI) const override {
+    reportUnimplementedError();
+  }
+
   MachineBasicBlock *generateBranch(InstructionGenerationContext &IGC,
                                     const MCInstrDesc &InstrDesc,
                                     MachineBasicBlock *Dst) const override {
