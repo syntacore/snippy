cc6a150f81f73150e790b587faf2856792da0f5b
Alexander Romanov <alexander.romanov@syntacore.com>
2026-08-27 11:11:50 +0000

    [snippy][test] mark all tests that need model to run
    Co-authored-by: Sergei Zimmerman <sergei.zimmerman@syntacore.com>

---
+++ llvm/tools/llvm-snippy/cmake/SnippyPlugins.cmake
@@ -0,0 +1,15 @@
+set(MODEL_PLUGIN_ORIGIN libRISCVModel.so)
+
+if (NOT SPIKE_MODEL_SRC)
+  set(SPIKE_MODEL_SRC ${RISCVModelSpike_DIR}/${MODEL_PLUGIN_ORIGIN})
+endif()
+set(RVM_SPIKE_PLUGIN riscv-spike-plugin.so)
+set(SPIKE_LIB_PATH ${LLVM_TOOLS_BINARY_DIR}/${RVM_SPIKE_PLUGIN})
+add_custom_command(
+  OUTPUT ${SPIKE_LIB_PATH}
+  MAIN_DEPENDENCY ${SPIKE_MODEL_SRC}
+  COMMAND ${CMAKE_COMMAND} -E copy ${SPIKE_MODEL_SRC} ${SPIKE_LIB_PATH}
+)
+add_custom_target(llvm-snippy-plugins DEPENDS
+  ${SPIKE_LIB_PATH}
+)
