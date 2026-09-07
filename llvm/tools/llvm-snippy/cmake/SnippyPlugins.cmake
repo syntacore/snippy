a279bda06ed2ddc73f6134345d487302e97cd7d5
Alexander Romanov <alexander.romanov@syntacore.com>
2026-09-07 12:40:12 +0000

    [snippy][test] add whisper-based tests

--- llvm/tools/llvm-snippy/cmake/SnippyPlugins.cmake
+++ llvm/tools/llvm-snippy/cmake/SnippyPlugins.cmake
@@ -8,12 +8,30 @@ if (NOT EXISTS "${SPIKE_MODEL_SRC}")
   message(FATAL_ERROR "RISCVModelSpike_DIR should point to a directory containing ${MODEL_PLUGIN_ORIGIN}}")
 endif()
 set(RVM_SPIKE_PLUGIN riscv-spike-plugin.so)
+
+if (NOT EXISTS "${RISCVModelWhisper_DIR}")
+  message(FATAL_ERROR "RISCVModelWhisper_DIR should be set to a valid directory")
+endif()
+set(WHISPER_MODEL_SRC ${RISCVModelWhisper_DIR}/${MODEL_PLUGIN_ORIGIN})
+if (NOT EXISTS "${WHISPER_MODEL_SRC}")
+  message(FATAL_ERROR "RISCVModelWhisper_DIR should point to a directory containing ${MODEL_PLUGIN_ORIGIN}}")
+endif()
+set(RVM_WHISPER_PLUGIN riscv-whisper-plugin.so)
+
 set(SPIKE_LIB_PATH ${LLVM_TOOLS_BINARY_DIR}/${RVM_SPIKE_PLUGIN})
+set(WHISPER_LIB_PATH ${LLVM_TOOLS_BINARY_DIR}/${RVM_WHISPER_PLUGIN})
 add_custom_command(
   OUTPUT ${SPIKE_LIB_PATH}
   MAIN_DEPENDENCY ${SPIKE_MODEL_SRC}
   COMMAND ${CMAKE_COMMAND} -E copy ${SPIKE_MODEL_SRC} ${SPIKE_LIB_PATH}
 )
+
+add_custom_command(
+  OUTPUT ${WHISPER_LIB_PATH}
+  MAIN_DEPENDENCY ${WHISPER_MODEL_SRC}
+  COMMAND ${CMAKE_COMMAND} -E copy ${WHISPER_MODEL_SRC} ${WHISPER_LIB_PATH}
+)
 add_custom_target(llvm-snippy-plugins DEPENDS
   ${SPIKE_LIB_PATH}
+  ${WHISPER_LIB_PATH}
 )
--- llvm/tools/llvm-snippy/package.nix
+++ llvm/tools/llvm-snippy/package.nix
@@ -101,7 +101,7 @@ stdenv.mkDerivation (finalAttrs: {
   '';
   checkPhase = ''
     runHook preCheck
-    for model in "None" "spike"; do
+    for model in "None" "spike" "whisper"; do
       export LIT_OPTS="-v --no-progress-bar -Dsnippy-test-model=$model"
       cmake --build . --target check-llvm-tools-llvm-snippy
     done
